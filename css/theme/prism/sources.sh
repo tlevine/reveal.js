8f460d3b402d0bfff4aecf1fff490ea518800d5e
Thomas Levine <occurrence@thomaslevine.com>
2013-09-02 20:57:04 +0000

    construct the gradient

--- css/theme/prism/sources.sh
+++ css/theme/prism/sources.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Download the sourced image files.
 
-wget http://commons.wikimedia.org/wiki/File%3ACover_slide_of_PRISM.jpg
+wget http://upload.wikimedia.org/wikipedia/commons/b/bd/Cover_slide_of_PRISM.jpg
