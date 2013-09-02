c241361508f5d110532be2006751d1f76dbe21c1
Thomas Levine <occurrence@thomaslevine.com>
2013-09-02 21:54:35 +0000

    cleaner shell script

--- css/theme/prism/sources.sh
+++ css/theme/prism/sources.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # Download the sourced image files.
+set -e
 
-wget http://upload.wikimedia.org/wikipedia/commons/b/bd/Cover_slide_of_PRISM.jpg
+test -e Cover_slide_of_PRISM.jpg || wget http://upload.wikimedia.org/wikipedia/commons/b/bd/Cover_slide_of_PRISM.jpg
